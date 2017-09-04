f3171b8df31493ef09cc3c7ec48197034c3315b8
Mojganx <mojgan.zolghadr@gmail.com>
2017-09-04 12:20:00 +0200

    added links to play and profile

--- src/components/NavDrawer.js
+++ src/components/NavDrawer.js
@@ -4,11 +4,12 @@ import MenuItem from 'material-ui/MenuItem';
 import Divider from 'material-ui/Divider';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import Menu from 'material-ui/svg-icons/navigation/menu';
+import {Link} from 'react-router'
 
 
 class NavDrawer extends Component {
   state = {
-    open: false
+    open: true
   }
 
   toggle = () =>{
@@ -41,13 +42,24 @@ class NavDrawer extends Component {
           >
           LoginContainer
         </div>
-        <Divider />
-          <MenuItem
-            primaryText={'Play'}
-          />
-          <MenuItem
-            primaryText={'Profile'}
-          />
+          <Divider />
+          <Link
+            to={'/'}
+          >
+            <MenuItem
+              onTouchTap={this.toggle}
+              primaryText={'Play'}
+            />
+          </Link>
+          <Link
+            to={'/profile'}
+          >
+            <MenuItem
+              onTouchTap={this.toggle}
+              primaryText={'Profile'}
+            />
+          </Link>
+
         </Drawer>
       </div>
     )
