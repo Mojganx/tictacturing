3879d907a941d89c3b3a0538210ba7bd9e9872e3
Mojganx <mojgan.zolghadr@gmail.com>
2017-09-05 15:02:46 +0200

    creating squares

--- src/containers/TicTacToe.js
+++ src/containers/TicTacToe.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {Stage} from 'react-konva'
-import {Board} from '../styled/TicTacToe'
+import {Board, Squares} from '../styled/TicTacToe'
 
 class TicTacToe extends Component {
 
@@ -21,17 +21,25 @@ class TicTacToe extends Component {
     let size = (height < width) ? height * .8 : width * .8
     let rows = this.state.rows
     let unit = size / rows
+    let coordinates = []
+    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < rows; x++){
+        coordinates.push([x*unit, y*unit])
+      }
+    }
 
     this.setState({
       size,
       rows,
-      unit
+      unit,
+      coordinates
     })
   }
 
 
 
-move = () => {
+move = (marker, index) => {
+  console.log('Move made', marker, index)
   //placeholder
 }
 
@@ -50,7 +58,13 @@ recordGame = () => {
     let {
       size,
       unit,
-      rows
+      rows,
+      coordinates,
+      gameState,
+      win,
+      gameOver,
+      yourTurn,
+      ownMark
     } = this.state
     return(
       <div>
@@ -63,7 +77,16 @@ recordGame = () => {
           rows={rows}
           size={size}
         />
-        {/* <Squares/> */}
+        <Squares
+        unit={unit}
+        coordinates={coordinates}
+        gameState={gameState}
+        win={win}
+        gameOver={gameOver}
+        yourTurn={yourTurn}
+        ownMark={ownMark}
+        move={this.move}
+        />
       </Stage>
 
       </div>
--- src/styled/TicTacToe.js
+++ src/styled/TicTacToe.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import {Layer, Line} from 'react-konva'
+import {Layer, Line, Text} from 'react-konva'
+
+
 
 export const Board = ({unit, size, rows}) => {
 
   let grid = []
   let stroke = 'grey'
   let strokeWidth = 10
-  for (let i =1; i < rows; i++) {
+  for (let i = 1; i < rows; i++) {
     let position = unit * i
     grid.push(
       <Line
@@ -29,6 +31,25 @@ export const Board = ({unit, size, rows}) => {
   return (
     <Layer>
       {grid}
+    </Layer>
+  )
+}
+
+export const Squares = ({
+  unit,
+  coordinates,
+  gameState,
+  win,
+  gameOver,
+  yourTurn,
+  ownMark,
+  move
+}) => {
+  return (
+    <Layer>
+      <Text
+
+      />
     </Layer>
   )
 }
